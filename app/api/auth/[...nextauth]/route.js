2d18c132fba6165bb27d8c7b627b312683b21c46
lis4x <lis4x1245@gmail.com>
2026-08-21 23:32:51 -0300

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 const GUILD_ID = process.env.DISCORD_GUILD_ID;
-const USMS_ROLE_ID = process.env.DISCORD_ROLE_ID;
 
 export const authOptions = {
   providers: [
@@ -18,14 +17,13 @@ export const authOptions = {
         token.accessToken = account.access_token;
       }
       try {
-        // Consultar a la API de Discord si el usuario está en el servidor y tiene el rol
         const response = await fetch(`https://discord.com/api/v10/users/@me/guilds/${GUILD_ID}/member`, {
           headers: { Authorization: `Bearer ${token.accessToken}` },
         });
         
         if (response.ok) {
           const member = await response.json();
-          token.hasRole = member.roles.includes(USMS_ROLE_ID);
+          token.hasRole = member.roles.includes("1092169208160325775");
         } else {
           token.hasRole = false;
         }
