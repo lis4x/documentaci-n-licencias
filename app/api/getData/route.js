4a7e56ca9d26144d7510b9eb8612f82250a67828
lis4x <lis4x1245@gmail.com>
2026-08-22 00:05:31 -0300

    Update route.js

--- app/api/getData/route.js
+++ app/api/getData/route.js
@@ -1,22 +1,20 @@
 import { google } from "googleapis";
 
+export const revalidate = 0;
+
 export async function GET() {
   try {
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        // Reemplaza saltos de línea codificados de las variables de Vercel
         private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       },
       scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
     });
 
     const sheets = google.sheets({ version: "v4", auth });
-
-    // ID extraído de tu enlace
     const spreadsheetId = "1M30awi9M5AGlz4cn8uIFh-pqBhLezHuxUGo92XLe94g";
 
-    // Si tu pestaña tiene un nombre específico (ejemplo: 'Hoja 1'), ponlo aquí antes del !
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId,
       range: "A1:Z1000",
