ac9cb54cbe484a9fde2857f4c470caac3d113657
lis4x <lis4x1245@gmail.com>
2026-08-21 23:42:47 -0300

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,35 +1,17 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-const GUILD_ID = process.env.DISCORD_GUILD_ID;
-
 export const authOptions = {
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
-      authorization: { params: { scope: "identify guilds.members.read" } },
+      authorization: { params: { scope: "identify" } },
     }),
   ],
   callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      try {
-        const response = await fetch(`https://discord.com/api/v10/users/@me/guilds/${GUILD_ID}/member`, {
-          headers: { Authorization: `Bearer ${token.accessToken}` },
-        });
-        
-        if (response.ok) {
-          const member = await response.json();
-          token.hasRole = member.roles.includes("1092169208160325775");
-        } else {
-          token.hasRole = false;
-        }
-      } catch (error) {
-        token.hasRole = false;
-      }
+    async jwt({ token }) {
+      token.hasRole = true;
       return token;
     },
     async session({ session, token }) {
