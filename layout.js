594447bdb61ebe27d795dfc011bc87df0becf6c8
lis4x <lis4x1245@gmail.com>
2026-08-21 23:04:22 -0300

    Create layout.js

---
+++ layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Base de Datos de Licencias',
+  description: 'Sistema de consulta de licencias de armas',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  )
+}
